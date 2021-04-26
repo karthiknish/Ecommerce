63b6c37541f0bd04523c41d22c7629832d46e1a5
Karthik Nishanth <karthik.nishanth06@gmail.com>
2021-04-26 10:06:07 +0530

    make it better

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,15 @@ import colors from "colors";
 // a mongoose stuf (mongoose.connect ....) return always a promise
 const connectDB = async () => {
   try {
-    const conn = await mongose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongose.connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://karthik:karthik@cluster0.sj4fz.mongodb.net/EcommercePfe?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
